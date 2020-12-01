e837343a93031540ceb8e80dad645d6408b2bf37
Justin Le <justin@jle.im>
2020-11-30 22:35:04 -0800

    generalized knapsack heh heh

--- src/AOC/Challenge/Day01.hs
+++ src/AOC/Challenge/Day01.hs
@@ -12,31 +12,39 @@ module AOC.Challenge.Day01 (
   , day01b
   ) where
 
+import           AOC.Common    (firstJust)
 import           AOC.Solver    ((:~>)(..))
 import           Control.Monad (guard)
-import           Data.List     (tails)
-import           Data.Maybe    (listToMaybe)
+import           Data.Set      (Set)
 import           Text.Read     (readMaybe)
+import qualified Data.Set      as S
+
+-- | Given a number @n@ of items and a goal sum and a set of numbers to
+-- pick from, finds the @n@ numbers in the set that add to the goal sum.
+knapsack
+    :: Int              -- ^ number of items n to pick
+    -> Int              -- ^ goal sum
+    -> Set Int          -- ^ set of options
+    -> Maybe [Int]      -- ^ resulting n items that sum to the goal
+knapsack 0 _    _  = Nothing
+knapsack 1 goal xs
+    | goal `S.member` xs = Just [goal]
+    | otherwise          = Nothing
+knapsack n goal xs = flip firstJust (S.toList xs) $ \x ->
+    let goal'   = goal - x
+        (_, ys) = S.split x xs
+    in  (x:) <$> knapsack (n - 1) goal' ys
 
 day01a :: [Int] :~> Int
 day01a = MkSol
     { sParse = traverse readMaybe . lines
     , sShow  = show
-    , sSolve = \xs -> listToMaybe do
-        x:ys <- tails xs
-        y    <- ys
-        guard (x + y == 2020)
-        pure (x*y)
+    , sSolve = fmap product . knapsack 2 2020 . S.fromList
     }
 
 day01b :: [Int] :~> Int
 day01b = MkSol
     { sParse = traverse readMaybe . lines
     , sShow  = show
-    , sSolve = \xs -> listToMaybe do
-        x:ys <- tails xs
-        y:zs <- tails ys
-        z    <- zs
-        guard (x + y + z == 2020)
-        pure (x*y*z)
+    , sSolve = fmap product . knapsack 3 2020 . S.fromList
     }
