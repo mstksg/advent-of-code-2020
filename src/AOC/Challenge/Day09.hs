82342df13e55cc051fad78066ebad108d021e585
Justin Le <justin@jle.im>
2020-12-08 23:17:44 -0800

    day 9 with recursion schemes

--- src/AOC/Challenge/Day09.hs
+++ src/AOC/Challenge/Day09.hs
@@ -12,25 +12,31 @@ module AOC.Challenge.Day09 (
   , day09b
   ) where
 
-import           AOC.Common     (slidingWindows)
-import           AOC.Solver     ((:~>)(..))
-import           Control.Monad  (guard)
-import           Data.Foldable  (toList)
-import           Data.List      (scanl', tails, find)
-import           Data.Sequence  (Seq(..))
-import           Text.Read      (readMaybe)
-import qualified Data.Vector    as V
+import           AOC.Common            (slidingWindows)
+import           Debug.Trace
+import           AOC.Solver            ((:~>)(..))
+import           Control.Monad         (guard, join)
+import           Data.Foldable         (toList)
+import           Data.Functor.Compose
+import           Data.Functor.Foldable
+import           Data.List             (scanl', tails, find)
+import           Data.Semigroup
+import           Data.Sequence         (Seq(..))
+import           Numeric.Natural
+import           Text.Read             (readMaybe)
+import qualified Data.Sequence as      Seq
+import qualified Data.Vector           as V
 
-isGood :: Seq Int -> Bool
-isGood (xs :|> x) = not . null $ do
+isBad :: Seq Int -> Bool
+isBad (xs :|> x) = null $ do
     y:ys <- tails (toList xs)
     z    <- ys
     guard $ (y + z) == x
-isGood _          = False
+isBad _          = True
 
 oddOneOut :: [Int] -> Maybe Int
 oddOneOut xs = do
-    _ :|> x <- find (not . isGood) (slidingWindows 26 xs)
+    _ :|> x <- find isBad (slidingWindows 26 xs)
     pure x
 
 day09a :: [Int] :~> Int
@@ -51,14 +57,35 @@ findBounds ns goal = go 0 1
         EQ -> pure (i, j)
         GT -> go (i + 1) j
 
+findBounds_ :: Seq Int -> Int -> Maybe (Int, Int)
+findBounds_ ns goal = case anaM @_ @Natural go (1, ns) of
+    (Nothing, _       ) -> Nothing
+    (Just (First d), i) -> Just (fromIntegral i, fromIntegral i+d)
+  where
+    go :: (Int, Seq Int) -> (Maybe (First Int), Maybe (Int, Seq Int))
+    go (!d, xs@(x :<| ys)) = case compare (y - x) goal of
+        LT -> go (d+1, xs)
+        EQ -> (Just (First d), Nothing)
+        GT -> (Nothing, Just (d-1, ys))
+      where
+        Just y = xs Seq.!? d
+    go _ = (Nothing, Nothing)
+
+anaM :: (Monad m, Corecursive t, Traversable (Base t)) => (a -> m (Base t a)) -> a -> m t
+anaM f = hylo (fmap embed . join . fmap sequenceA . getCompose) (Compose . f)
+
 day09b :: [Int] :~> (Int, Int)
 day09b = MkSol
     { sParse = traverse readMaybe . lines
     , sShow  = \(x,y) -> show (x + y)
     , sSolve = \ns -> do
         goal   <- oddOneOut ns
+        traceM "hi"
+        traceM $ show goal
         let cumsum = V.fromList (scanl' (+) 0 ns)
         (i, j) <- findBounds cumsum goal
+        traceM $ show (V.head cumsum, V.last cumsum)
+        traceM $ show (i, j, findBounds_ (Seq.fromList (V.toList cumsum)) goal)
         let xs = take (j - i) . drop i $ ns
         pure (minimum xs, maximum xs)
     }
