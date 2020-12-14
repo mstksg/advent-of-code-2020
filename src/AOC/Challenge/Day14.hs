2c349ef33fec54b6223bbc5f2946abf6b8d84816
Justin Le <justin@jle.im>
2020-12-13 21:29:01 -0800

    literally raw day14

--- src/AOC/Challenge/Day14.hs
+++ src/AOC/Challenge/Day14.hs
@@ -26,7 +26,7 @@ module AOC.Challenge.Day14 (
   , day14b
   ) where
 
-import           AOC.Prelude
+import           AOC.Prelude hiding (binary)
 
 import qualified Data.Graph.Inductive           as G
 import qualified Data.IntMap                    as IM
@@ -37,6 +37,7 @@ import qualified Data.List.PointedList.Circular as PLC
 import qualified Data.Map                       as M
 import qualified Data.OrdPSQ                    as PSQ
 import qualified Data.Sequence                  as Seq
+import           Numeric.Lens
 import qualified Data.Set                       as S
 import qualified Data.Text                      as T
 import qualified Data.Vector                    as V
@@ -47,14 +48,41 @@ import qualified Text.Megaparsec.Char.Lexer     as PP
 
 day14a :: _ :~> _
 day14a = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . fst . foldl' process (IM.empty, replicate 36 Nothing)
     }
+  where
+    process :: (IntMap Int, [Maybe Bool]) -> String -> (IntMap Int, [Maybe Bool])
+    process (mp,mks) str = case words str of
+      "mask":_:bs:_ -> (mp, map (\case '0' -> Just False; '1' -> Just True; 'X' -> Nothing) bs)
+      (takeWhile isDigit.drop 4->memer):_:n:_ ->
+        let digitstr = zipWith go mks (printf "%036b" (read @Int n))
+            val = digitstr ^?! binary
+        in  (IM.insert (read memer) val mp, mks)
+    go Nothing = id
+    go (Just True) = const '1'
+    go (Just False) = const '0'
+
+
+
+
 
 day14b :: _ :~> _
 day14b = MkSol
     { sParse = sParse day14a
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . fst . foldl' process (IM.empty, replicate 36 Nothing)
     }
+  where
+    process :: (IntMap Int, [Maybe Bool]) -> String -> (IntMap Int, [Maybe Bool])
+    process (mp,mks) str = case words str of
+      "mask":_:bs:_ -> (mp, map (\case '0' -> Just False; '1' -> Just True; 'X' -> Nothing) bs)
+      (takeWhile isDigit.drop 4->memer):_:n:_ ->
+        let memers :: [[Char -> Char]]
+            memers = for mks \case
+              Nothing -> [const '0', const '1']
+              Just True -> [const '1']
+              Just False -> [id]
+            ixes = map (fromJust . preview binary . zipWith (&) (printf "%036b" (read @Int memer))) memers
+        in  (foldl' (\mm x -> IM.insert x (read n) mm) mp ixes, mks)
