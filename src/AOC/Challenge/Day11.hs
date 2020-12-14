3ab0f7ffddf395a65329c7a5db3885f25ae50ddb
Justin Le <justin@jle.im>
2020-12-14 01:52:13 -0800

    day 11 using mutable vectors

--- src/AOC/Challenge/Day11.hs
+++ src/AOC/Challenge/Day11.hs
@@ -12,24 +12,27 @@ module AOC.Challenge.Day11 (
   , day11b
   ) where
 
-import           AOC.Common                (Point, boundingBox', inBoundingBox, fullNeighbs, fullNeighbsSet, parseAsciiMap, countTrue, loopMaybe)
-import           AOC.Solver                ((:~>)(..))
-import           Control.Monad             (guard)
-import           Control.Monad.State       (State, modify, runState)
-import           Data.Bit                  (Bit(..))
-import           Data.Bits                 (popCount)
-import           Data.Finite               (Finite)
-import           Data.Foldable             (find, toList)
-import           Data.Map                  (Map)
-import           Data.Maybe                (mapMaybe)
-import           Data.Set                  (Set)
-import           GHC.TypeNats              (KnownNat)
-import           Linear                    (V2(..))
-import qualified Data.Map.Strict           as M
-import qualified Data.Set                  as S
-import qualified Data.Vector.Generic.Sized as VG
-import qualified Data.Vector.Sized         as V
-import qualified Data.Vector.Unboxed.Sized as VU
+import           AOC.Common                        (Point, boundingBox', inBoundingBox, fullNeighbs, fullNeighbsSet, parseAsciiMap, countTrue)
+import           AOC.Solver                        ((:~>)(..))
+import           Control.Monad.Loops               (whileM_)
+import           Control.Monad.Primitive           (PrimMonad, PrimState)
+import           Control.Monad.ST                  (runST)
+import           Control.Monad.State               (execStateT, modify, StateT)
+import           Data.Bit                          (Bit(..))
+import           Data.Bits                         (popCount)
+import           Data.Finite                       (Finite, finites)
+import           Data.Foldable                     (find, toList, traverse_)
+import           Data.Map                          (Map)
+import           Data.Maybe                        (mapMaybe)
+import           Data.Set                          (Set)
+import           GHC.TypeNats                      (KnownNat)
+import           Linear                            (V2(..))
+import qualified Data.Map.Strict                   as M
+import qualified Data.Set                          as S
+import qualified Data.Vector.Generic.Sized         as VG
+import qualified Data.Vector.Sized                 as V
+import qualified Data.Vector.Unboxed.Mutable.Sized as MVU
+import qualified Data.Vector.Unboxed.Sized         as VU
 
 parseSeatMap :: String -> Map Point Bool
 parseSeatMap = parseAsciiMap $ \case
@@ -49,31 +52,40 @@ compile mp f = V.withSizedList (toList mp) $ \xs ->
     go = fromIntegral . (`M.findIndex` mp)
 
 seatRule
-    :: forall n. ()
-    => Int                       -- ^ exit seat threshold
-    -> V.Vector n [Finite n]
-    -> VU.Vector n Bit
-    -> Maybe (VU.Vector n Bit)
-seatRule thr ns xs = res <$ guard changed
+    :: forall n m s. (KnownNat n, PrimMonad m, s ~ PrimState m)
+    => Int                          -- ^ exit seat threshold
+    -> V.Vector n [Finite n]        -- ^ neighbors
+    -> MVU.MVector n s Bit          -- ^ source
+    -> MVU.MVector n s Bit          -- ^ target
+    -> StateT Bool m ()             -- ^ changed
+seatRule thr ns src targ = traverse_ go finites
   where
-    (res, changed) = runState (VU.imapM go xs) False
-    go :: Finite n -> Bit -> State Bool Bit
-    go i (Bit x)
-        | x == x'   = pure $ Bit x
-        | otherwise = Bit x' <$ modify (const True)
-      where
-        n = countTrue (unBit . (xs `VU.index`)) $ ns `V.index` i
-        x' = case x of
-          False -> not (n > 0)
-          True  -> not (n >= thr)
+    go :: Finite n -> StateT Bool m ()
+    go i = do
+      Bit x <- MVU.read src i
+      n     <- countTrue unBit <$> traverse (MVU.read src) (ns `V.index` i)
+      let x' = case x of
+            False -> not (n > 0)
+            True  -> not (n >= thr)
+      modify (|| (x /= x'))
+      MVU.write targ i (Bit x')
+    {-# INLINE go #-}
+{-# INLINE seatRule #-}
 
 solveWith
     :: KnownNat n
     => Int                      -- ^ exit seat threshold
-    -> V.Vector n [Finite n]
+    -> V.Vector n [Finite n]    -- ^ neighbors
     -> VU.Vector n Bit
     -> Int                      -- ^ equilibrium size
-solveWith thr ns = popCount . loopMaybe (seatRule thr ns)
+solveWith thr ns xs = runST $ do
+    xs0 <- VU.thaw xs
+    xs1 <- MVU.unsafeNew
+    (`whileM_` pure ()) . flip execStateT False $ do
+      seatRule thr ns xs0 xs1
+      seatRule thr ns xs1 xs0
+    popCount <$> VU.unsafeFreeze xs0
+
 
 -- | Get a map of points to all of those points' neighbors where there is
 -- a seat. Should only need to be computed once.
