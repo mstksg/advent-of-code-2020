99e35dbda39bfe7bbb033f1fa5f85f086c90398c
Justin Le <justin@jle.im>
2020-12-14 23:07:30 -0800

    more rb progress

--- src/AOC/Common/RedBlack.hs
+++ src/AOC/Common/RedBlack.hs
@@ -47,37 +47,66 @@ leafBranch
     => MutBranch s (RBTree k a) ()
 leafBranch = constrMB #_Leaf
 
+data TNE a b = TNE !b ![(a, b)]
+
+insTNE :: a -> b -> TNE a b -> TNE a b
+insTNE x y (TNE z zs) = TNE y ((x,z):zs)
+
 treeInsert
     :: forall m s k a. (PrimMonad m, PrimState m ~ s, Mutable s k, Mutable s a, Ord k)
     => k
     -> a
     -> Ref s (RBTree k a)
-    -> m (NonEmpty (Ref s (NodeData k a)))   -- trace of ancestors
+    -> m (TNE Bool (Ref s (NodeData k a)))
+    -- -> m (NonEmpty (Ordering, Ref s (NodeData k a)))   -- trace of ancestors
     -- -> m (NonEmpty (Ref s (RBTree k a)))   -- trace of ancestors
     -- -> m [Ref s (RBTree k a)]   -- trace of ancestors
 treeInsert k x = go1
   where
-    go1 :: Ref s (RBTree k a) -> m (NonEmpty (Ref s (NodeData k a)))
+    go1 :: Ref s (RBTree k a) -> m (TNE Bool (Ref s (NodeData k a)))
     go1 tRef = projectBranch nodeBranch tRef >>= \case
       Nothing -> do
         ndRef <- thawRef $ NodeData k Red Leaf x Leaf
-        (ndRef :| []) <$ moveBranch nodeBranch tRef ndRef
+        TNE ndRef [] <$ moveBranch nodeBranch tRef ndRef
       Just ndRef -> go2 ndRef
-    go2 :: Ref s (NodeData k a) -> m (NonEmpty (Ref s (NodeData k a)))
+    go2 :: Ref s (NodeData k a) -> m (TNE Bool (Ref s (NodeData k a)))
     go2 ndRef = do
       k' <- freezePart (fieldMut #ndKey) ndRef
       case compare k k' of
-        LT -> (ndRef NE.<|) <$> go1 (getMutPart (fieldMut #ndLeft ) ndRef)
-        EQ -> (ndRef  :|[]) <$  copyPart (fieldMut #ndVal) ndRef x
-        GT -> (ndRef NE.<|) <$> go1 (getMutPart (fieldMut #ndRight) ndRef)
-
+        LT -> (insTNE False ndRef) <$> go1 (getMutPart (fieldMut #ndLeft ) ndRef)
+        EQ -> TNE ndRef []      <$  copyPart (fieldMut #ndVal) ndRef x
+        GT -> (insTNE True  ndRef) <$> go1 (getMutPart (fieldMut #ndRight) ndRef)
 
 restore
     :: (PrimMonad m, PrimState m ~ s, Mutable s k, Mutable s a, Ord k)
-    => NonEmpty (Ref s (NodeData k a))
+    => TNE Bool (Ref s (NodeData k a))
     -> m ()
 restore = \case
-    ndRef :| [] -> copyPart (fieldMut #ndColor) ndRef Black
+    TNE ndRef []              -> copyPart (fieldMut #ndColor) ndRef Black
+    TNE ndRef ((cmp,pRef):ps) -> case cmp of
+      -- this is the left branch, so uncle is the right
+      -- NO this is wrong, that's the brother not the uncle UGH D:
+      False -> do
+        let uncle = getMutPart (fieldMut #ndRight) pRef
+        c <- projectBranch nodeBranch uncle >>= \case
+          Nothing   -> pure Black
+          Just uRef -> freezePart (fieldMut #ndColor) uRef
+        pure ()
+      -- this is the right branch, so uncle is the left
+      True  -> undefined
+    
+      
+    -- ndRef :| []        -> copyPart (fieldMut #ndColor) ndRef Black
+    -- ndRef :| (pRef:ps) -> do
+      
+-- restore
+--     :: (PrimMonad m, PrimState m ~ s, Mutable s k, Mutable s a, Ord k)
+--     => NonEmpty (Ref s (NodeData k a))
+--     -> m ()
+-- restore = \case
+--     ndRef :| []        -> copyPart (fieldMut #ndColor) ndRef Black
+--     ndRef :| (pRef:ps) -> do
+      
     -- projectBranch nodeBranch t >>= \case
     --   Nothing -> error "hm.."
     --   Just (_, cRef, _, _, _) -> copyRef cRef Black
