9913d1106a5be0b23c0f4876bc55d505be427c6b
Justin Le <justin@jle.im>
2020-12-15 09:38:19 -0800

    cleaned up day 15

--- src/AOC/Challenge/Day15.hs
+++ src/AOC/Challenge/Day15.hs
@@ -14,33 +14,32 @@ module AOC.Challenge.Day15 (
 
 import           AOC.Solver                  ((:~>)(..))
 import           Control.Monad.Loops         (whileM_)
-import           Control.Monad.ST            (ST, runST)
+import           Control.Monad.ST            (runST)
 import           Control.Monad.State.Strict  (StateT(..), evalStateT, gets)
-import           Data.Foldable               (traverse_)
-import           Data.List.NonEmpty          (NonEmpty(..))
+import           Data.Foldable               (for_)
 import           Data.List.Split             (splitOn)
 import           Text.Read                   (readMaybe)
-import qualified Data.List.NonEmpty          as NE
 import qualified Data.Vector.Unboxed.Mutable as MV
 
-day15a :: NonEmpty Int :~> Int
+day15a :: [Int] :~> Int
 day15a = MkSol
-    { sParse = (NE.nonEmpty =<<) . traverse readMaybe . splitOn ","
+    { sParse = traverse readMaybe . splitOn ","
     , sShow  = show
     , sSolve = Just . looper 2020
     }
 
-day15b :: NonEmpty Int :~> Int
+day15b :: [Int] :~> Int
 day15b = MkSol
     { sParse = sParse day15a
     , sShow  = show
     , sSolve = Just . looper 30000000
     }
 
-looper :: Int -> NonEmpty Int -> Int
-looper n xs0 = runST $ flip evalStateT (0,NE.head xs0) $ do
+looper :: Int -> [Int] -> Int
+looper n xs0 = runST $ flip evalStateT (0,0) $ do
     v <- MV.replicate n 0
-    traverse_ (initter v) xs0
+    for_ xs0 $ \y -> StateT $ \(!i,_) ->
+      (,(i+1, y)) <$> MV.write v y (i+1)
     whileM_ (gets ((< n) . fst)) $ StateT $ \(!i, !x) -> do
       lst <- MV.read v x
       MV.write v x i
@@ -48,8 +47,3 @@ looper n xs0 = runST $ flip evalStateT (0,NE.head xs0) $ do
             | otherwise = i - lst
       pure ((),(i+1, j))
     gets snd
-  where
-    initter :: MV.MVector s Int -> Int -> StateT (Int, Int) (ST s) ()
-    initter v y = StateT $ \(!i, _) ->
-      (,(i+1, y)) <$> MV.write v y (i+1)
-    {-# INLINE initter #-}
