bb42c1146c78f555b3ecaa4a8b42612ed102c54b
Justin Le <justin@jle.im>
2020-12-17 15:10:50 -0800

    cleanup symmetry

--- src/AOC/Challenge/Day17.hs
+++ src/AOC/Challenge/Day17.hs
@@ -10,52 +10,54 @@ module AOC.Challenge.Day17 (
   , day17b
   ) where
 
-import           AOC.Common      (fullNeighbsSet, (!!!), asciiGrid)
-import           Debug.Trace
-import           Data.Foldable (toList)
-import           Control.Applicative (liftA2)
-import           AOC.Solver      ((:~>)(..))
-import           Control.Lens
--- import           Control.Lens    (to, set, asIndex, filtered)
-import           Data.Set        (Set)
-import           Data.Set.Lens   (setOf)
-import           Linear          (R2(..), V3(..), V4(..))
-import qualified Data.Map.Strict as M
-import qualified Data.Set        as S
+import           AOC.Common          (fullNeighbsSet, (!!!), asciiGrid)
+import           AOC.Solver          ((:~>)(..))
+import           Control.Lens        (iover, traversed, set, to, asIndex, filtered)
+import           Data.Foldable       (toList)
+import           Data.Set            (Set)
+import           Data.Set.Lens       (setOf)
+import           Linear              (R2(..), V3(..), V4(..))
+import qualified Data.Map.Strict     as M
+import qualified Data.Set            as S
 
 stepper
-    :: (Applicative f, Num a, Ord (f a), Eq a, Traversable f, R2 f)
+    :: (Applicative f, Num a, Ord (f a), Ord a, Traversable f)
     => Set (f a)
     -> Set (f a)
 stepper cs = stayAlive <> comeAlive
   where
     neighborCounts = M.unionsWith ((+) @Int) $
-      [ M.fromSet (weight c) (S.map (abszy) (fullNeighbsSet c))
+      [ M.fromSet (weight c) (S.map abszy (fullNeighbsSet c))
       | c <- S.toList cs
       ]
     stayAlive = M.keysSet . M.filter (\n -> n == 2 || n == 3) $
                   neighborCounts `M.restrictKeys` cs
     comeAlive = M.keysSet . M.filter (== 3) $
                   neighborCounts `M.withoutKeys`  cs
-    weight x y = product (zipWith (\i j -> if i == 1 && j == 0 then 2 else 1) xs ys)
-      where
-        xs = drop 2 (toList x)
-        ys = drop 2 (toList y)
-    abszy x = set _xy (view _xy x) (fmap abs x)
+    abszy = iover traversed (\i -> if i > 1 then abs else id)
+{-# INLINE stepper #-}
+
+weight
+    :: (Foldable f, Num a, Ord a)
+    => f a
+    -> f a
+    -> Int
+weight x y = product $
+    zipWith go (drop 2 (toList x)) (drop 2 (toList y))
+  where
+    go i j
+      | i > 0 && j == 0 = 2
+      | otherwise       = 1
+{-# INLINE weight #-}
 
 day17
-    :: (Applicative f, R2 f, Ord (f Int), Traversable f, Show (f Int))
+    :: (Applicative f, R2 f, Ord (f Int), Traversable f)
     => Set (f Int) :~> Int
 day17 = MkSol
     { sParse = Just . parseMap
     , sShow  = show
-    , sSolve = Just . S.size . foldMap dupSym  . (!!! 6) . iterate stepper
+    , sSolve = Just . sum . M.fromSet (`weight` pure 0)  . (!!! 6) . iterate stepper
     }
-  where
-    dupSym x = S.fromList
-      [ liftA2 (*) x p
-      | p <- set _xy 1 <$> sequence (pure [1,-1])
-      ]
 {-# INLINE day17 #-}
 
 day17a :: Set (V3 Int) :~> Int
@@ -72,4 +74,3 @@ parseMap = setOf $ asciiGrid
                  . filtered (== '#')
                  . asIndex
                  . to (\p -> set _xy p (pure 0))
-
