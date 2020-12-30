f2afc2753bd96e5e60217060d9df48d8086b3c36
Justin Le <justin@jle.im>
2020-12-29 23:06:42 -0800

    sqlite3 cache for day 17

--- src/AOC/Challenge/Day17.hs
+++ src/AOC/Challenge/Day17.hs
@@ -1,5 +1,4 @@
-{-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
-{-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- |
 -- Module      : AOC.Challenge.Day17
@@ -21,21 +20,27 @@ import           AOC.Common                  ((!!!), factorial, freqs, lookupFre
 import           AOC.Common.Point            (Point, parseAsciiSet)
 import           AOC.Solver                  ((:~>)(..))
 import           Control.DeepSeq             (force, NFData)
+import           Control.Monad               (unless)
 import           Control.Monad.ST            (runST)
+import           Data.Bifunctor              (second)
 import           Data.Coerce                 (coerce)
 import           Data.Foldable               (toList, for_)
 import           Data.IntMap.Strict          (IntMap)
 import           Data.IntSet                 (IntSet)
 import           Data.List                   (scanl', sort)
+import           Data.List.Split             (chunksOf)
 import           Data.Set                    (Set)
 import           GHC.Generics                (Generic)
 import           Linear                      (V2(..))
+import           System.IO.Unsafe            (unsafePerformIO)
+import           Text.Printf                 (printf)
 import qualified Data.IntMap                 as IM
 import qualified Data.IntMap.Monoidal.Strict as MIM
 import qualified Data.IntSet                 as IS
 import qualified Data.Set                    as S
 import qualified Data.Vector                 as V
 import qualified Data.Vector.Mutable         as MV
+import qualified Database.SQLite.Simple      as D
 
 pascals :: [[Int]]
 pascals = repeat 1 : map (tail . scanl' (+) 0) pascals
@@ -137,11 +142,6 @@ neighborWeights
 neighborWeights d mx = runST $ do
     v <- MV.replicate n IM.empty
     for_ [0 .. n-1] $ \x ->
-      -- let nmap = IM.fromListWith (<>)
-      --          . map (\i -> (pascalIx (sort i), NOne))
-      --          $ neighbs mx (ixPascal d x)
-      -- for_ (IM.toList nmap) $ \(i, c) ->
-      --   MV.unsafeModify v (IM.insert x c) i
       for_ (neighbs mx (ixPascal d x)) $ \i -> do
         MV.unsafeModify v (IM.insertWith (flip (<>)) x NOne) $
           pascalIx (sort i)
@@ -198,8 +198,7 @@ day17 d = MkSol
             nxy     = bounds + 12
             shifted = IS.fromList $
                 (\(V2 i j) -> i + j * nxy) . (+ 6) <$> x
-            wts = force $ neighborWeights d 6
-            -- wts = force $ neighborWeightsNoCache d 6 x
+            wts = unsafePerformIO $ loadNeighborWeights d 6
         in  Just . sum
                  . IM.fromSet (finalWeight d . drop 2 . ixDouble d nxy)
                  . (!!! 6)
@@ -227,8 +226,88 @@ day17b = day17 2
 -- d=11: ? / 309176832; 43m54s  -- with unboxed, 5m3s, with pre-neighb: 1m43s (no cache: 4.5s)
 --                                      smallcache: 52s
 -- d=12: ? / 1537981440 -- with unboxed, 22m10s, with pre-neighb: 8m30s (no cache: 7.4s)
+-- d=13: ? / 7766482944 -- sqlite3 cache: 13.4s
 
 parseMap
     :: String
     -> Set Point
 parseMap = parseAsciiSet (== '#')
+
+neighborPairs
+    :: Int    -- ^ dimension
+    -> Int    -- ^ maximum
+    -> [(Int, Int)]
+neighborPairs d mx =
+    [ (pascalIx (sort i), x)
+    | x <- [0 .. n-1]
+    , i <- neighbs mx (ixPascal d x)
+    ]
+  where
+    n = pascals !! d !! mx
+
+cacheNeighborWeights
+    :: D.Connection
+    -> Int    -- ^ dimension
+    -> Int    -- ^ maximum
+    -> IO ()
+cacheNeighborWeights conn d mx = do
+    D.executeNamed conn
+      "DELETE FROM cache WHERE dim = :d" [ ":d" D.:= d ]
+    for_ (chunkUp <$> chunksOf 10_000_000 (neighborPairs d mx)) $ \pmap -> do
+      let chunky   = IM.size pmap
+          bunky    = sum $ IM.size <$> pmap
+          lastSeen = fromIntegral $ maximum (fst . IM.findMax <$> pmap) + 1
+      printf "[%05.2f%%] Cacheing chunk of size %d/%d ...\n" (lastSeen / n * 100) chunky bunky
+      D.withTransaction conn $ D.executeMany conn
+        "INSERT INTO cache(dim,source,target,weight) VALUES (?,?,?,?) ON CONFLICT(dim,source,target) DO UPDATE SET weight = weight + ? WHERE weight < 4"
+        [ (d, x, y, c, c)
+        | (x, ys) <- IM.toList pmap
+        , (y, z ) <- IM.toList ys
+        , let c = fromCount z
+        ]
+  where
+    fromCount :: NCount -> Int
+    fromCount = \case
+      NOne   -> 1
+      NTwo   -> 2
+      NThree -> 3
+      NMany  -> 4
+    n :: Double
+    n = fromIntegral (pascals !! d !! mx)
+    chunkUp = IM.fromListWith (IM.unionWith (<>))
+            . (map . second) (`IM.singleton` NOne)
+
+loadCache
+    :: D.Connection
+    -> Int      -- ^ dimensions
+    -> Int      -- ^ max
+    -> IO (V.Vector (IntMap NCount))
+loadCache conn d mx =
+    V.generateM n $ \src -> do
+      fmap toCount . IM.fromList <$> D.queryNamed conn
+        "SELECT target,weight FROM cache WHERE dim = :d AND source = :src"
+        [ ":d" D.:= d, ":src" D.:= src ]
+  where
+    n = pascals !! d !! mx
+    toCount :: Int -> NCount
+    toCount = \case
+      1 -> NOne
+      2 -> NTwo
+      3 -> NThree
+      _ -> NMany
+
+loadNeighborWeights
+    :: Int    -- ^ dimensions
+    -> Int    -- ^ maximum
+    -> IO (V.Vector (IntMap NCount))
+loadNeighborWeights d mx = D.withConnection "cache/day17.db" $ \conn -> do
+    D.execute_ conn
+      "CREATE TABLE IF NOT EXISTS cache (dim INT, source INT, target INT, weight INT, CONSTRAINT dst UNIQUE(dim, source, target))"
+    exists <- not . null @[] @(D.Only Int) <$> D.queryNamed conn
+      "SELECT dim FROM cache WHERE dim = :d LIMIT 1" [":d" D.:= d]
+    unless exists $ do
+      putStrLn "Building cache..."
+      cacheNeighborWeights conn d mx
+      putStrLn "Cache completed."
+    loadCache conn d mx
+      -- <* putStrLn "cache loaded"
